4587c8a2223088661891e2964e6e2b3d1942bc30
adminint <43671456+adminint@users.noreply.github.com>
2021-11-04 16:52:43 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,9 +8,9 @@ else
     rm -rf $INSTALL_DIR
 fi 
 
-cp -f send_mail.sh snapshot_lxc_publish_all.sh snapshot_lxc_publish_one.sh download_from_onedrive.sh restore_lxc_from_backup.sh /usr/local/bin/lxd-backup
-chmod a+x /usr/local/bin/lxd-backup/snapshot_lxc_publish_all.sh
-chmod a+x /usr/local/bin/lxd-backup/send_mail.sh
-chmod a+x /usr/local/bin/lxd-backup/snapshot_lxc_publish_one.sh
-chmod a+x /usr/local/bin/lxd-backup/download_from_onedrive.sh
-chmod a+x /usr/local/bin/lxd-backup/restore_lxc_from_backup.sh
+cp -f send_mail.sh snapshot_lxc_publish_all.sh snapshot_lxc_publish_one.sh download_from_onedrive.sh restore_lxc_from_backup.sh $INSTALL_DIR
+chmod a+x $INSTALL_DIR/snapshot_lxc_publish_all.sh
+chmod a+x $INSTALL_DIR/send_mail.sh
+chmod a+x $INSTALL_DIR/snapshot_lxc_publish_one.sh
+chmod a+x $INSTALL_DIR/lxd-backup/download_from_onedrive.sh
+chmod a+x $INSTALL_DIR/lxd-backup/restore_lxc_from_backup.sh
