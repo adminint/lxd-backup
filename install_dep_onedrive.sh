cc25c8096b45ac2de84e05c4258d39f5e854d513
adminint <43671456+adminint@users.noreply.github.com>
2021-11-04 15:00:34 +0300

    Update install_dep_onedrive.sh

--- install_dep_onedrive.sh
+++ install_dep_onedrive.sh
@@ -3,7 +3,7 @@
 export http_proxy=http://93.123.128.147:8118
 export https_proxy=http://93.123.128.147:8118
 
-apt install libcurl4 libcurl4-openssl-dev curl pkg-config libsqlite3-dev -y
+apt install libcurl4 libcurl4-openssl-dev curl pkg-config libsqlite3-dev swaks -y
 curl -fsS https://dlang.org/install.sh | bash -s dmd
 
 source ~/dlang/dmd-2.098.0/activate
