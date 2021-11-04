c061df80be181dae96f5c68a7d72217f5b6a20b8
adminint <43671456+adminint@users.noreply.github.com>
2021-11-04 16:57:46 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,13 +4,12 @@ INSTALL_DIR="/usr/local/bin/lxd-backup"
 if [ ! -d $INSTALL_DIR ] 
 then
     mkdir $INSTALL_DIR -p
-else
-    rm -rf $INSTALL_DIR
 fi 
+
 
 cp -f send_mail.sh snapshot_lxc_publish_all.sh snapshot_lxc_publish_one.sh download_from_onedrive.sh restore_lxc_from_backup.sh $INSTALL_DIR
 chmod a+x $INSTALL_DIR/snapshot_lxc_publish_all.sh
 chmod a+x $INSTALL_DIR/send_mail.sh
 chmod a+x $INSTALL_DIR/snapshot_lxc_publish_one.sh
-chmod a+x $INSTALL_DIR/lxd-backup/download_from_onedrive.sh
-chmod a+x $INSTALL_DIR/lxd-backup/restore_lxc_from_backup.sh
+chmod a+x $INSTALL_DIR/download_from_onedrive.sh
+chmod a+x $INSTALL_DIR/restore_lxc_from_backup.sh
