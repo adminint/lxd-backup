dfe708ae4562603b223229444b5e3eefeef2d009
adminint <43671456+adminint@users.noreply.github.com>
2021-11-04 17:12:58 +0300

    Update send_mail.sh

--- send_mail.sh
+++ send_mail.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 NAME=`/bin/cat /usr/local/bin/backup.log |/bin/grep CONTAINER |awk {'print $2'} |/bin/sed 's/=/ /g' |awk {'print $2'}`
-/usr/bin/swaks --to gebs@infolink.ru --from "smart-intercom@infolink.ru" --header "Subject: ALL-LXD-BACKUP: container - $NAME"  --body $1 \
---server mail.infolink.ru \
---port 25 --timeout 40s --auth LOGIN --auth-user "smart-intercom@infolink.ru" \
---auth-password "*********"
+/usr/bin/swaks --to YOU_EMAIL@domain.com --from "backup@domain.com" --header "Subject: ALL-LXD-BACKUP: container - $NAME"  --body $1 \
+--server mail.domain.com \
+--port 25 --timeout 40s --auth LOGIN --auth-user "backup@domain.com" \
+--auth-password "MAIL_PASSWORD"
 > /usr/local/bin/backup.log
